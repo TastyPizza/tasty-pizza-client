d390ed6263c00c8046f379e6b47b954fcdbfe87d
Daniil <twilightnotch@gmail.com>
2023-05-07 19:12:58 +0300

    Update authorization form

--- app/src/main/java/com/example/tastypizzaclient/profile/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/tastypizzaclient/profile/fragment/RegisterFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import com.example.tastypizzaclient.MainActivity
 import com.example.tastypizzaclient.R
 import com.google.android.material.textfield.TextInputLayout
 import java.util.*
@@ -21,14 +22,18 @@ class RegisterFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
         emailInput = view.findViewById(R.id.email_input_sign_up)
-        val loginButton  = view.findViewById<Button>(R.id.registration_button_sign_up)
-        loginButton.setOnClickListener {
+        val registerButton  = view.findViewById<Button>(R.id.registration_button_sign_up)
+        registerButton.setOnClickListener {
             val email = emailInput.editText?.text.toString()
             println(email)
         }
         val picker = view.findViewById<Button>(R.id.show_date_picker_button)
+        val loginButton  = view.findViewById<Button>(R.id.authorization_button_sign_up)
+        loginButton.setOnClickListener {
+            switchRegisterOnLogin()
+        }
 //        picker.setOnClickListener {
 //            val currentDate = Calendar.getInstance()
 //            val year = currentDate.get(Calendar.YEAR)
@@ -43,11 +48,15 @@ class RegisterFragment : Fragment() {
 //        }
         return view
     }
-
+    private fun switchRegisterOnLogin(){
+        val fragment = LoginFragment.newInstance()
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
     companion object {
-
         @JvmStatic
-        fun newInstance() = LoginFragment()
-
+        fun newInstance() = RegisterFragment()
     }
 }
