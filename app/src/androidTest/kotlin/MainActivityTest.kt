e05ebba64fec3aa417d84bf18436c1344a80f97a
Daniil <twilightnotch@gmail.com>
2023-05-25 12:03:32 +0300

    Working version of Authorization, big change in all project and add retrofit

--- app/src/androidTest/kotlin/MainActivityTest.kt
+++ app/src/androidTest/kotlin/MainActivityTest.kt
@@ -8,7 +8,6 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import com.example.tastypizzaclient.MainActivity
 import com.example.tastypizzaclient.R
 import org.junit.Rule
